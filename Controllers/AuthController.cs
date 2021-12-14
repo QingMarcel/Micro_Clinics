a5025bc76783846798c1e87e984678e843063cba
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2021-12-14 11:07:09 +0000

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroClinic.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterPatient()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterDoctor()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SecuredController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroClinic.Controllers
+{
+    public class SecuredController : Controller
+    {
+        public IActionResult Patient()
+        {
+            return View();
+        }
+
+        public IActionResult Doctor()
+        {
+            return View();
+        }
+    }
+}
